73bd9c8423902832e115a0cd928d09d65a9a1c4a
Yvan Tchoutouo <ytchouotouo@utecq.dev>
2024-08-27 21:17:11 +0100

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/user/header/header.component";
 import { TranslateService } from '@ngx-translate/core';
 import { MainComponent } from "./components/user/main/main.component";
+import { FooterComponent } from './components/user/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainComponent],
+  imports: [RouterOutlet, HeaderComponent, MainComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/user/footer/footer.component.ts
+++ src/app/components/user/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { getSiteName } from '../../../helpers/helper';
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +8,11 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  siteName: string ="";
+  
+  ngOnInit(): void {
+    this.siteName = getSiteName();
+  }
 
 }
