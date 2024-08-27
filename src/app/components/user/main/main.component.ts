d20af3d49de205a0a0f630b684a8f49a9af9f42a
Cedric Asofack <casofackzeu@utecq.dev>
2024-08-27 20:31:26 +0200

    correction

--- src/app/components/user/main/main.component.ts
+++ src/app/components/user/main/main.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from "../banner/banner.component";
 import { ProductsHightlightComponent } from "../products-hightlight/products-hightlight.component";
+import { ProductListComponent } from "../product-list/product-list.component";
+import { CategoriesListComponent } from "../categories-list/categories-list.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [BannerComponent, ProductsHightlightComponent],
+  imports: [BannerComponent, ProductsHightlightComponent, ProductListComponent, CategoriesListComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
