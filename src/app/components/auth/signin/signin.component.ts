d81d9606cebbdb49b95540914a518ec42d39a31d
Cedric Asofack <casofackzeu@utecq.dev>
2024-08-30 20:05:17 +0200

    product details and sign in - web version conflits gérés

--- src/app/components/auth/signin/signin.component.ts
+++ src/app/components/auth/signin/signin.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { getSiteName } from '../../../helpers/helper';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, RouterLink],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
