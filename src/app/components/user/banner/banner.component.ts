0836e83fb55df16b6cb2e059339351f7f2b78095
Yvan Tchoutouo <ytchouotouo@utecq.dev>
2024-08-31 15:48:56 +0100

    translate banner

--- src/app/components/user/banner/banner.component.ts
+++ src/app/components/user/banner/banner.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-banner',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css'
 })
