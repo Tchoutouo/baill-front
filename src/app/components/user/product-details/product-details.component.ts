0b7cf7f7b80f185b88a3f00ddc103185ce23e81c
Cedric Asofack <casofackzeu@utecq.dev>
2024-08-31 19:40:25 +0200

    gestion responsive pages : login, home-page et product-details

--- src/app/components/user/product-details/product-details.component.ts
+++ src/app/components/user/product-details/product-details.component.ts
@@ -12,6 +12,7 @@ import { ProductComponent } from "../product/product.component";
 export class ProductDetailsComponent {
     imagesList : Array<string> = ['']; 
     number_limit : Array<number> = [1, 3, 4, 5] ;
+    fisrtImage : string = ''
 
     constructor(){
 
@@ -20,8 +21,17 @@ export class ProductDetailsComponent {
     ngOnInit() {
         this.imagesList = ["../../../../assets/products/prodcut-1/1.jpeg", "../../../../assets/products/prodcut-1/2.jpeg", 
            "../../../../assets/products/prodcut-1/3.png",  "../../../../assets/products/prodcut-1/4.jpeg",
-        ]
+        ];
+
+        this.reChangeImage();
     }
 
+    changeFirstImage(event : any){
+      this.fisrtImage = event;
+    }
+
+    reChangeImage(){
+      this.fisrtImage = this.imagesList[0]
+    }
 
 }
