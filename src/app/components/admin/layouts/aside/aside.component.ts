e7c1b366483d6726fe939f899dfefbbf2a5fd9fd
Cedric Asofack <casofackzeu@utecq.dev>
2024-09-25 21:55:21 +0200

    comportement web sidebar

--- src/app/components/admin/layouts/aside/aside.component.ts
+++ src/app/components/admin/layouts/aside/aside.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 import { getSiteName } from '../../../../helpers/helper';
 import { TranslateModule } from '@ngx-translate/core';
 import { RouterLink, RouterModule } from '@angular/router';
@@ -16,9 +16,43 @@ export class AsideComponent implements OnInit{
   userName: string ="";
   email: string ="";
 
+  @Input() sideBarIsOpen : boolean = false ;
+
+  displayShow : string = "d-none";
+  sizeAside : string = ""
+  card : string = "col-12 justify-content-center"
+  styleImageProf = "shadow-profile";
+  width_full : string = "";
+  list_width : string = "";
+  loggout_content : string = "justify-content-center"
+
   ngOnInit(): void {
-    this.siteName = getSiteName();
+    this.siteName = getSiteName();  
     this.userName = "Marlane Halle";
     this.email = "halle@gmail.com";
   }
+
+
+  ngOnChanges(changes: SimpleChanges): void {
+    
+    if (this.sideBarIsOpen) {
+      this.displayShow = " ";
+      this.sizeAside = "onpenAside"
+      this.card = "card"
+      this.styleImageProf = ""
+      this.width_full = "w_100"
+      this.list_width = "w-100"
+      this.loggout_content = "justify-content-end p-0"
+    }else{
+      this.displayShow = "d-none"
+      this.sizeAside = " "
+      this.card = "col-12 justify-content-center"
+      this.styleImageProf = "shadow-profile"
+      this.width_full =" "
+      this.list_width = " "
+      this.loggout_content = "justify-content-center"
+    }
+  }
+
+  
 }
