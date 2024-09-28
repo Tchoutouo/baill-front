2291637d1422c36d812c2a8d7a0b626a50490ca1
Yvan Tchoutouo <ytchouotouo@utecq.dev>
2024-09-28 08:03:26 +0100

    signin traduction

--- src/app/components/auth/signup/signup.component.ts
+++ src/app/components/auth/signup/signup.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { getSiteName } from '../../../helpers/helper';
 import { TranslateModule } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, RouterLink],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
