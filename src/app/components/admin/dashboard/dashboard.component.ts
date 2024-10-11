9f5ff51c26350bdd913edc620c2a05fbd64056a2
Cedric Asofack <casofackzeu@utecq.dev>
2024-10-11 21:48:08 +0200

    mise à jour code

--- src/app/components/admin/dashboard/dashboard.component.ts
+++ src/app/components/admin/dashboard/dashboard.component.ts
@@ -1,16 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AlertComponent } from "../alert/alert.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AlertComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
 
-    anounceList : Array<any> = []
+    anounceList : Array<any> = [];
+
+    message_alert : any  = null;
 
     constructor(){
 
