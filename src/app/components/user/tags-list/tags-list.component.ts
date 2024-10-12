452b780285a69d70fb651eeb1d1ce634ac71f107
Yvan Tchoutouo <ytchouotouo@utecq.dev>
2024-10-12 15:34:09 +0100

    catéroy

--- src/app/components/user/tags-list/tags-list.component.ts
+++ src/app/components/user/tags-list/tags-list.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { City, Country, ICity, ICountry } from 'country-state-city';
+import { Category } from '../../../models/admin/category';
+import { EntityServiceService } from '../../../services/admin/entity-service.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tags-list',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './tags-list.component.html',
   styleUrl: './tags-list.component.css'
 })
-export class TagsListComponent {
+export class TagsListComponent implements OnInit{
+  selectedCountry: string = '';
+  selectedCity: string = '';
+  countries: ICountry[] | undefined;
+  cities: ICity[] | undefined;
+  categories: Category[] = [] ;
+
+  constructor(private entityService : EntityServiceService){}
+  
+  ngOnInit(): void {
+    this.countries = Country.getAllCountries();
+    this.getAllCategories();
+  }
+
+  async onCountryChange(event: any) {
+    try{
+      const countryCode = event.target.value;
+      this.cities = City.getCitiesOfCountry(countryCode);
+
+    } catch (error) {
+      console.error('Erreur lors de la récupération des villes:', error);
+    }
+  }
+
+  async getAllCategories(){
+    try{
+      const entity= "categorie_back";
+      this.entityService.getAll(entity)
+      .subscribe({
+        next: (categories: any)=>{
+          this.categories = categories
+        },
+        error: (error: any)=>{
+          console.log("error", error)
+        },
+        complete: ()=>{
+          console.log("complete")
+        }
+      })
+
+    }catch (e){
+      console.error('erreur de recupérer des catégories d\'articles:', e)
+    }
+  }
 
 }
---
+++ src/app/models/admin/category.ts
@@ -0,0 +1,5 @@
+export interface Category {
+    id?: BigInteger,
+    title: string,
+    description?: string
+}
