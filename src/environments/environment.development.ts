7c05f75a658968cc2cff4d8668f60dc0f02ba202
Yvan Tchoutouo <ytchouotouo@utecq.dev>
2024-11-13 17:17:28 +0100

    integration backend

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,10 @@
 export const environment = {
     production : false,
     // apiUrl: "http://localhost:8000/api/",
-    apiUrl: "http://baill-back.sc1zeucoder.universe.wf/api/",
-    apiUrlRessources: "http://baill-back.sc1zeucoder.universe.wf/public/storage"
+    // apiUrl: "http://baill-back.sc1zeucoder.universe.wf/api/",
+    // apiUrlRessources: "http://baill-back.sc1zeucoder.universe.wf/public/storage"
+    apiUrl: "https://facebook.audiencespub.com/api/",
+    apiUrlRessources: "https://facebook.audiencespub.com/public/storage"
     // apiUrl: "https://test-backend.bailleurnet.com/api/"
 };  
 // http://localhost:8000/autres_projets/bailleur/baill-back/public/storage/images/tOvffNjuvVWg06Y4gU62qW9fDzl6o19PImKCGwDK.jpg
