1359b36e02f8fe3d091e1e4273daa9afa60f4bf9
Yvan Tchoutouo <yvalttnomdecode@gmail.com>
2025-02-03 12:40:37 +0100

    work flow inscription

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,8 @@
 export const environment = {
     production : false,
+    // apiUrl: "http://127.0.0.1:8000/api/",
+    // apiUrlRessources: "http://127.0.0.1:8000/public/storage"
+
     apiUrl: "https://backend.bailleurnet.com/api/",
     apiUrlRessources: "https://backend.bailleurnet.com/public/storage"
 
