58a6da9c1e93988479ca601faeebf5d3cff5094b
Yvan Tchoutouo <yvalttnomdecode@gmail.com>
2025-05-21 15:27:46 +0700

    save change devices to XAF

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withDebugTracing } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -8,8 +8,11 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { LoadingInterceptor } from './interceptors/loading.interceptor';
 import { CorsInterceptor } from './interceptors/cors.interceptor';
 import { provideNgxStripe, STRIPE_PUBLISHABLE_KEY } from 'ngx-stripe';
+import { registerLocaleData } from '@angular/common';
 
+import localeFrCM from '@angular/common/locales/fr-CM';
 
+registerLocaleData(localeFrCM);
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
@@ -17,6 +20,7 @@ export function createTranslateLoader(http: HttpClient) {
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    { provide: LOCALE_ID, useValue: 'fr-CM' },
     importProvidersFrom([
       TranslateModule.forRoot({
         loader: {
--- src/app/components/admin/forfait-item/forfait-item.component.ts
+++ src/app/components/admin/forfait-item/forfait-item.component.ts
@@ -1,12 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormatTimePipe } from "../../../pipes/format-time.pipe";
 import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-forfait-item',
   standalone: true,
-  imports: [FormatTimePipe,TranslateModule],
+  imports: [FormatTimePipe,TranslateModule, CommonModule],
   templateUrl: './forfait-item.component.html',
   styleUrl: './forfait-item.component.css'
 })
