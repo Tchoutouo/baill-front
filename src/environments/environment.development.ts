b19ebd860c6d824530215bea0da20c6465bc9853
Yvan Tchoutouo <yvalttnomdecode@gmail.com>
2025-07-30 16:32:13 +0700

    save

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production : false,
-    apiUrl: "http://127.0.0.1:8000/api/",
-    apiUrlRessources: "http://127.0.0.1:8000/public/storage"
+    // apiUrl: "http://127.0.0.1:8000/api/",
+    // apiUrlRessources: "http://127.0.0.1:8000/public/storage"
 
     //ENV for development
     // apiUrl: "https://test-backend.bailleurnet.com/api/",
@@ -12,8 +12,8 @@ export const environment = {
     //apiUrlRessources: "https://backend.bailleurnet.com/public/storage"
  
     //ENV for production
-    //apiUrl: "https://backend-prod.bailleurnet.com/api/",
-    //apiUrlRessources: "https://backend-prod.bailleurnet.com/public/storage"
+    apiUrl: "https://backend-prod.bailleurnet.com/api/",
+    apiUrlRessources: "https://backend-prod.bailleurnet.com/public/storage"
     
     // apiUrl: "http://baill-back.sc1zeucoder.universe.wf/api/",
     // apiUrlRessources: "http://baill-back.sc1zeucoder.universe.wf/public/storage"
